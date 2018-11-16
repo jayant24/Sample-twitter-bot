ceed24b3e626d305cdef47e0926e4dac9c7fb840
jayant24 <31399779+jayant24@users.noreply.github.com>
2018-11-16 14:12:43 +0530

    Add files via upload

---
+++ keys_format.py
@@ -0,0 +1,9 @@
+# keys_format.py shares the same format as keys.py.
+# This file is not meant to be used in the main script.
+# Instead, copy this file into keys.py and put your own
+# # keys there.
+
+# CONSUMER_KEY = 'cC8XpTSrvqVFr2mahN90O28Vd'
+# CONSUMER_SECRET = 'iuc2oZGrc0Yb0WHwHsLKwo7A5Q2yhfqxIawG4gWrDYviaBMTyP'
+# ACCESS_KEY = '819212305766481920-mll2kbqQ5XpjwZHX1n0b24QQTXOjkXO'
+# ACCESS_SECRET = 'JVUCDTdQ2B0EnPo2dWhR4oDQwcM1CgM3Jd7Ngn65J0h18'
